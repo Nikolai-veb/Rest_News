aa9c024493a578670ff5f9341fc4dc15f447995c
Nikloay <nik.vagin1995@mail.ru>
2021-03-26 23:45:32 +0300

    Small optimization

--- news/views.py
+++ news/views.py
@@ -7,13 +7,13 @@ from . import serializer
 class ListArticleView(ListAPIView):
     """List Articles View"""
     serializer_class = serializer.ListArticleSerializers
-    queryset = Articles.objects.filter(moderation=True)
+    queryset = Articles.objects.filter(moderation=True).only('user', 'title', 'create', 'moderation')
 
 
 class DetailArticleView(RetrieveAPIView):
     """Detail Article View"""
     serializer_class = serializer.DetailArticleSerializer
-    queryset = Articles.objects.filter(moderation=True)
+    queryset = Articles.objects.filter(moderation=True).defer('update',)
     lookup_field = 'slug'
 
 
@@ -27,7 +27,7 @@ class CreateArticleView(CreateAPIView):
 class UpdateArticleView(UpdateAPIView):
     """Update Article View"""
     serializer_class = serializer.UpdateArticleSerializer
-    queryset = Articles.objects.all()
+    queryset = Articles.objects.filter(moderation=True).only('title', 'text')
     permission_classes = [permissions.IsAuthenticated]
     lookup_field = 'slug'
 
@@ -35,6 +35,6 @@ class UpdateArticleView(UpdateAPIView):
 class DeleteArticleView(DestroyAPIView):
     """Delete Article View"""
     serializer_class = serializer.DeleteArticleSerializer
-    queryset = Articles.objects.all()
+    queryset = Articles.objects.filter(modertion=True)
     permission_classes = [permissions.IsAuthenticated]
     lookup_field = 'slug'
